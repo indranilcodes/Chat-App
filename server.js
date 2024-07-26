47c7b7b54d978ff9184c12cdb362a27a20f3c93c
indranilcodes <indranilrsvm03@gmail.com>
2024-07-26 23:11:38 +0530

    cors policy

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "*",
+    origin: "https://indranil-chatapp.vercel.app",
     //credentials: true,
   },
 });
